49cd1452251bdb8f3cbc21bcbb35bb095a214f30
Mika <majstormapp@gmail.com>
2022-03-24 23:27:06 +0100

    Solved leap year problem

---
+++ leap/leap.go
@@ -0,0 +1,10 @@
+// Package leap implements leap year checks
+package leap
+
+// IsLeapYear should have a comment documenting it.
+func IsLeapYear(year int) bool {
+	if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
+		return true
+	}
+	return false
+}
